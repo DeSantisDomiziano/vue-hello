16c0af95d7aecc72176fd7f9319a28d1f9502850
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-03-28 17:01:03 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            title: 'Boolean',
+            imgSrc: 'https://yt3.googleusercontent.com/UXFC9eFKxjbOcUeEiIFqpywdInXJijIrh5bkfUMPlKhCLKaO6rrTfh5O5IzNTS_2Wap2yBk1J44=s900-c-k-c0x00ffffff-no-rj',
+        }
+    }
+}).mount('#app')
